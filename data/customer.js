84f0923f49cdeef1c20b9cfd3815c7040e800c80
Andre Alves <andresistinfo@gmail.com>
2022-02-05 22:26:01 +0000

    Adding the data sample folder

---
+++ data/customer.js
@@ -0,0 +1,22 @@
+const customers = [
+    {
+        id: 1,
+        name: 'Andre',
+        age: 36,
+        authPass: '11111111'
+    },
+    {
+        id: 2,
+        name: 'Ju',
+        age: 35,
+        authPass: '22222222'
+    },
+    {
+        id: 3,
+        name: 'New User',
+        age: 10,
+        authPass: '33333333'
+    }
+]
+
+export default customers;
